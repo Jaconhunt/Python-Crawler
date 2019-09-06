98d5ff9307f3ccb76d792678cd6570792a686ac3
flipped*zq <32381162+Jaconhunt@users.noreply.github.com>
2019-09-06 15:16:17 +0800

    Update Python Crawler.py

--- Python Crawler.py
+++ Python Crawler.py
@@ -205,3 +205,35 @@ print(result)
 data = ['http', 'www.baidu.com', 'index.html',  'a = 6', 'comment']
 print(result.scheme, result[0])
 print(urlunsplit(data))
+
+from urllib.parse import urlencode
+
+params = {
+    'name': 'germey',
+    'age': 22
+}
+
+base_url = 'http://www.baidu.com?'
+url = base_url +urlencode(params)
+print(url)
+
+from urllib.parse import parse_qs
+
+query = 'name=germey&age=22'
+print(parse_qs(query))
+
+from urllib.parse import parse_qsl
+
+query = 'name=germey&age=22'
+print(parse_qsl(query))
+
+from urllib.parse import quote
+
+keyword = '壁纸'
+url = 'https://www.baidu.com/s?wd='+quote(keyword)
+print(url)
+
+from urllib.parse import unquote
+
+url = 'https://www.baidu.com/s?wd=%E5%A3%81%E7%BA%B8'
+print(unquote(url))
