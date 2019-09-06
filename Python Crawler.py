a09724d8e146ae88177310d25ce95dec0895e3d4
flipped*zq <32381162+Jaconhunt@users.noreply.github.com>
2019-09-06 17:41:15 +0800

    Update Python Crawler.py

--- Python Crawler.py
+++ Python Crawler.py
@@ -278,3 +278,35 @@ data = {
 
 r = requests.get('http://httpbin.org/get', params= data)
 print(r.text)
+
+import requests
+
+r= requests.get("http://httpbin.org/get")
+print(type(r.text))
+print(r.json())
+print(type(r.json()))
+
+import requests
+import re
+
+headers = {
+    'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36\
+            (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
+}
+r = requests.get('https://www.zhihu.com/explore', headers=headers)
+pattern = re.compile('explore-feed.*?question_link.*?>(.*?)</a>', re.S)
+titles = re.findall(pattern, r.text)
+print(titles)
+#无结果，知乎限制
+
+import requests
+
+r = requests.get("https://github.com/favicon.ico")
+print(r.text)
+print(r.content)
+
+import requests
+
+r = requests.get("https://github.com/favicon.ico")
+with open('favicon.ico', 'wb') as f:
+    f.write(r.content)
