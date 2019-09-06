3b60cc8a1752ca006dc38091a43225fb68684d95
flipped*zq <32381162+Jaconhunt@users.noreply.github.com>
2019-09-06 11:54:03 +0800

    Update Python Crawler.py

--- Python Crawler.py
+++ Python Crawler.py
@@ -146,3 +146,14 @@ except error.HTTPError as e:
     print(e.reason, e.code, e.headers, seq= '\n')
 # 上面代码有误,应该去除seq = '\n'
 # 'seq' is an invalid keyword argument for this function
+
+from  urllib import request, error
+
+try:
+    response = request.urlopen('https://cuiqingcai.com/index.htm')
+except error.HTTPError as e:
+    print(e.reason, e.code, e.headers)
+except error.URLError as e:
+    print(e.reason)
+else:
+    print('Request Successfully')
