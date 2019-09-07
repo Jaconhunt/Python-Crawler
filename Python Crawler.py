e8bb200ce158d85b7c6556dfba87c0215d94e9c1
flipped*zq <32381162+Jaconhunt@users.noreply.github.com>
2019-09-07 22:15:10 +0800

    Update Python Crawler.py

--- Python Crawler.py
+++ Python Crawler.py
@@ -356,3 +356,31 @@ print(result)
 print(result.group())
 print(result.group(1))
 print(result.span())
+
+#通用匹配
+import re
+
+content = 'Hello 1234567 World_This is a Regex Demo'
+print(len(content))
+result = re.match('^Hello.*Demo$',content)
+print(result)
+print(result.group())
+print(result.span())
+
+#贪婪与非贪婪
+import re
+
+content = 'Hello 1234567 World_This is a Regex Demo'
+print(len(content))
+result = re.match('^He.*(\d+).*Demo$',content)
+print(result)
+print(result.group(1))  #贪婪匹配
+
+import re
+
+content = 'Hello 1234567 World_This is a Regex Demo'
+print(len(content))
+result = re.match('^He.*?(\d+).*Demo$',content)
+print(result)
+print(result.group(1)) #非贪婪匹配
+
