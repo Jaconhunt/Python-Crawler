5dc630973b6209fcefb097cd873b8ac4b108a11e
flipped*zq <32381162+Jaconhunt@users.noreply.github.com>
2019-09-07 23:49:23 +0800

    Update Python Crawler.py

--- Python Crawler.py
+++ Python Crawler.py
@@ -392,11 +392,11 @@ result2 = re.match('^http.*?comment/(.*)',content)
 print('result1', result1.group(1))
 print('result2', result2.group(1)) #注意末尾匹配情况
 
-#修饰符，源代码有误
+#修饰符
 import re
 
-content = 'Hello 1234567 World_This \
-is a Regex Demo'
+content = '''Hello 1234567 World_This
+is a Regex Demo'''
 result = re.match('^He.*?(\d+).*?Demo$',content, re.S)
 print(result.group(1))
 
@@ -407,10 +407,66 @@ content = '(百度)www.baidu.com'
 result = re.match('\(百度\)www\.baidu\.com', content)
 print(result)
 
-#search(）
+#search(）实例1
 import re
 
 content = 'Extra stings Hello 1234567 World_This is a Regex Demo Extra stings'
 result =re.search('Hello.*?(\d+).*?Demo', content)
 print(result)
 
+#search(）实例2
+import re
+
+html = '''<div id="songs-list">
+    <h2 class="title">经典老歌</h2>
+    <p class="introduction">
+        经典老歌列表
+    </p>
+    <ul id="list" class="list-group">
+        <li data-view="2">一路上有你</li>
+        <li data-view="7">
+            <a href="/2.mp3" singer="任贤齐">沧海一声笑</a>
+        </li>
+        <li data-view="4" class="active">
+            <a href="/3.mp3" singer="齐秦">往事随风</a>
+        </li>
+        <li data-view="6"><a href="/4.mp3" singer="beyond">光辉岁月</a></li>
+        <li data-view="5"><a href="/5.mp3" singer="陈慧琳">记事本</a></li>
+        <li data-view="5">
+            <a href="/6.mp3" singer="邓丽君">但愿人长久</a>
+        </li>
+    </ul>
+</div>'''
+
+result = re.search('<li.*?active.*?singer="(.*?)">(.*?)</a>',html,re.S)
+print(result.group(1),result.group(2))
+
+result = re.search('<li.*?singer="(.*?)">(.*?)</a>',html,re.S)
+print(result.group(1),result.group(2))
+
+result = re.search('<li.*?singer="(.*?)">(.*?)</a>',html) #不匹配换行符
+print(result.group(1),result.group(2))
+
+results = re.findall('<li.*?href="(.*?)".*?singer="(.*?)">(.*?)</a>',html,re.S)
+print(results)
+print(type(results))
+for result in results:
+    print(result)
+    print(result[0], result[1], result[2])
+
+results = re.findall('<li.*?>\s*?(<a.*?>)?(\w+)(</a>)?\s*?</li>', html, re.S)
+for result in results:
+    print(result[1])
+
+html = re.sub('<a.*?>|</a>', '', html)
+print(html)
+results = re.findall('<li.*?>(.*?)</li>', html, re.S)
+for result in results:
+    print(result.strip()) #sub实例2
+
+#sub()
+import re
+
+content =  '54aK54yr5oiR54ix5L2g'
+content = re.sub('\d+', '', content)
+print(content)
