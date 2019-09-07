eabe23778338047f452436acac491974072b7618
flipped*zq <32381162+Jaconhunt@users.noreply.github.com>
2019-09-07 23:55:54 +0800

    Update Python Crawler.py

--- Python Crawler.py
+++ Python Crawler.py
@@ -470,3 +470,16 @@ import re
 content =  '54aK54yr5oiR54ix5L2g'
 content = re.sub('\d+', '', content)
 print(content)
+
+#compile()
+import re
+
+content1 = '2016-12-15 12:00'
+content2 = '2016-12-17 12:55'
+content3 = '2016-12-22 13:21'
+
+pattern = re.compile('\d{2}:\d{2}')
+result1 = re.sub(pattern, '', content1)
+result2 = re.sub(pattern, '', content2)
+result3 = re.sub(pattern, '', content3)
+print(result1, result2, result3)
