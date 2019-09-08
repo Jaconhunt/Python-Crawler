2ad1cda53a8e54889f98ed8798e9b3a975f98bfc
flipped*zq <32381162+Jaconhunt@users.noreply.github.com>
2019-09-08 23:05:57 +0800

    Update Python Crawler.py

--- Python Crawler.py
+++ Python Crawler.py
@@ -483,3 +483,28 @@ result1 = re.sub(pattern, '', content1)
 result2 = re.sub(pattern, '', content2)
 result3 = re.sub(pattern, '', content3)
 print(result1, result2, result3)
+
+#lxml实例引入
+from lxml import etree
+
+text = '''
+<div>
+    <ul>
+         <li class="item-0"><a href="link1.html">first item</a></li>
+         <li class="item-1"><a href="link2.html">second item</a></li>
+         <li class="item-1"><a href="link4.html">fourth item</a></li>
+         <li class="item-0"><a href="link5.html">fifth item</a> # 注意，此处缺少一个 </li> 闭合标签
+     </ul>
+ </div>
+'''
+
+html = etree.HTML(text)
+result = etree.tostring(html)
+print(result.decode('utf-8'))
+#待补充其他lxml用法
+
+#BeautifulSoup
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup('<p>Hello</p>', 'lxml')
+print(soup.p.string)
