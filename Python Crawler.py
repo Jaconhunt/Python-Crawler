f22a2150467e26cdffd261b61fa88fbf43e02806
flipped*zq <32381162+Jaconhunt@users.noreply.github.com>
2019-09-09 17:45:23 +0800

    Update Python Crawler.py

--- Python Crawler.py
+++ Python Crawler.py
@@ -1,6 +1,11 @@
-'''
-Create by: Jacon Hunt
-'''
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019-9-8 13:37
+# @Author  : JaocnHunt
+# @Site    : 
+# @File    : Python_100.py
+# @Software: PyCharm
+
 import urllib.request
 
 response = urllib.request.urlopen('https://www.python.org')
