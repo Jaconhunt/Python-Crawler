b576476320f5228c60dd26debf9180eea40a3c6b
flipped*zq <32381162+Jaconhunt@users.noreply.github.com>
2019-09-09 16:52:05 +0800

    Update Python Crawler.py

--- Python Crawler.py
+++ Python Crawler.py
@@ -530,3 +530,249 @@ for item in items:
     file.write('\n' + '=' * 50 + '\n')
 file.close()
 #此种方式无法得到内容，可能被反爬虫
+
+#读取json
+import json
+
+str = '''
+[{
+"name": "Bob",
+"Gender": "male",
+"Birthday": "1992-10-18"
+},{"name": "Selina",
+"Gender": "female",
+"Birthday": "1995-10-18"
+}
+]
+'''
+print(type(str))
+data = json.loads(str)
+print(data)
+print(type(data))
+print(data[0]['name'])
+print(data[0].get('name'))
+print(data[0].get('age'))
+print(data[0].get('age', 25))
+#https://stackoverflow.com/questions/39491420/python-jsonexpecting-property-name-enclosed-in-double-quotes
+
+#json文本读取
+import json
+
+with open('data.json','r') as file:
+    str = file.read()
+    data = json.loads(str)
+    print(data)
+    
+#输出json
+import json
+
+data = [{
+"name": "王伟",
+"Gender": "男",
+"Birthday": "1992-10-18"
+},{"name": "Selina",
+"Gender": "female",
+"Birthday": "1995-10-18"
+}
+]
+with open ('data.json', 'w', encoding = 'utf-8') as file:
+    file.write(json.dumps(data, indent =2, ensure_ascii = False))
+ 
+#CSV文件存储
+import csv
+
+with open('data.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['id', 'name', 'age'])
+    writer.writerow(['10001', 'Mike', 20])
+    writer.writerow(['10002', 'Bob', 22])
+    writer.writerow(['10003', 'Jordan', 21])
+    
+#字典写入
+import csv
+
+with open('data.csv', 'w') as csvfile:
+    fieldnames= ['id', 'name', 'age']
+    writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
+    writer.writeheader()
+    writer.writerow({'id':'10001','name': 'Mike','age':20})
+    writer.writerow({'id':'10002','name': 'Bob','age': 22})
+    writer.writerow({'id':'10003', 'name':'Jordan','age': 21})
+    writer.writerow({'id': '10004', 'name': 'Durant', 'age': 22})
+ 
+#中文写入
+import csv
+
+with open('data.csv', 'a', encoding = 'utf-8') as csvfile:
+    fieldnames= ['id', 'name', 'age']
+    writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
+    writer.writerow({'id': '10005', 'name': '王伟', 'age': 22})
+    
+#读取CSV
+import csv
+
+with open('data.csv', 'r', encoding ='utf-8') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        print(row)
+
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+print(df)
+
+#连接数据库MySQL
+import pymysql
+
+db = pymysql.connect(host = 'localhost', user = 'root', password = 'czq123xyz', port = 3306)
+cursor = db.cursor()
+cursor.execute('SELECT VERSION()')
+data = cursor.fetchone()
+print('Database version:', data)
+cursor.execute("CREATE DATABASE spiders1 DEFAULT CHARACTER SET utf8MB4")
+db.close()
+
+#创建表
+import pymysql
+
+db = pymysql.connect(host = 'localhost', user = 'root', password = 'czq123xyz', port = 3306, db = 'spiders1')
+cursor = db.cursor()
+sql = 'CREATE TABLE IF NOT EXISTS students (id VARCHAR(255) NOT  NULL, name VARCHAR(255) NOT NULL ,age INT NOT NULL ,PRIMARY KEY(id))'
+cursor.execute(sql)
+db.close()
+
+#插入数据
+import pymysql
+
+id = '2010001'
+user = 'Bob'
+age = 20
+
+db = pymysql.connect(host = 'localhost', user = 'root', password = 'czq123xyz', port = 3306, db = 'spiders1')
+cursor = db.cursor()
+sql = 'INSERT INTO students(id, name, age) values(%s, %s, %s)'
+try:
+    cursor.execute(sql, (id, user, age))
+    db.commit()
+except:
+    db.rollback()
+db.close()
+
+#动态表
+import pymysql
+
+db = pymysql.connect(host = 'localhost', user = 'root', password = 'czq123xyz', port = 3306, db = 'spiders1')
+cursor = db.cursor()
+data = {
+    'id':  '2010001',
+    'name':  'Bob',
+    'age': 20
+}
+table = 'students'
+keys = ','.join(data.keys())
+values = ','.join(['%s']*len(data))
+sql = 'INSERT INTO {table}({keys}) VALUES ({values})'.format(table =table, keys =keys, values = values)
+try:
+    if cursor.execute(sql, tuple(data.values())):
+        print('Successful')
+        db.commit()
+except:
+    print('Failed')
+    db.rollback()
+db.close()
+
+#更新数据
+import pymysql
+
+db = pymysql.connect(host = 'localhost', user = 'root', password = 'czq123xyz', port = 3306, db = 'spiders1')
+cursor = db.cursor()
+sql = 'UPDATE students SET age = %s WHERE name = %s'
+try:
+    cursor.execute(sql, (25, 'Bob'))
+    db.commit()
+except:
+    db.rollback()
+db.close()
+
+#去除重复值
+import pymysql
+
+db = pymysql.connect(host = 'localhost', user = 'root', password = 'czq123xyz', port = 3306, db = 'spiders1')
+cursor = db.cursor()
+data = {
+    'id':  '2010001',
+    'name':  'Bob',
+    'age': 21
+}
+table = 'students'
+keys = ','.join(data.keys())
+values = ','.join(['%s']*len(data))
+sql = 'INSERT INTO {table}({keys}) VALUES ({values}) ON DUPLICATE KEY UPDATE '.format(table =table, keys =keys, values = values)
+update = ','.join(["{key} = %s".format(key =key ) for key in data])
+sql += update
+try:
+    if cursor.execute(sql, tuple(data.values())*2):
+        print('Successful')
+        db.commit()
+except:
+    print('Failed')
+    db.rollback()
+db.close()
+
+#删除数据
+import pymysql
+
+db = pymysql.connect(host = 'localhost', user = 'root', password = 'czq123xyz', port = 3306, db = 'spiders1')
+cursor = db.cursor()
+table = 'students'
+condition = 'age > 20'
+
+sql = 'DELETE FROM {table} WHERE {condition}'.format(table = table, condition = condition)
+try:
+    cursor.execute(sql)
+    db.commit()
+except:
+    db.rollback()
+db.close()
+
+#查询数据
+import pymysql
+
+db = pymysql.connect(host = 'localhost', user = 'root', password = 'czq123xyz', port = 3306, db = 'spiders1')
+cursor = db.cursor()
+table = 'students'
+
+sql = 'SELECT * FROM students WHERE  age >= 20'
+
+try:
+    cursor.execute(sql)
+    print('Count:', cursor.rowcount)
+    one = cursor.fetchone()
+    print('One:', one)
+    results = cursor.fetchall()
+    print('Results:', results)
+    print('Results Type:', type(results))
+    for row in results:
+        print(row)
+except:
+    print('Error')
+#共用指针, cursor
+
+#While循环遍历数据
+import pymysql
+
+db = pymysql.connect(host = 'localhost', user = 'root', password = 'czq123xyz', port = 3306, db = 'spiders1')
+cursor = db.cursor()
+table = 'students'
+
+sql = 'SELECT * FROM students WHERE  age >= 20'
+
+try:
+    cursor.execute(sql)
+    print('Count:', cursor.rowcount)
+    row = cursor.fetchone()
+    while row:
+        print('Row:', row)
+        row = cursor.fetchone()
+except:
+    print('Error')
