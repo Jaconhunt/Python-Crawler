dc7c33ae602beb923ef7a581e3b6b43a8c68ea24
flipped*zq <32381162+Jaconhunt@users.noreply.github.com>
2019-09-10 16:17:26 +0800

    Update Python Crawler.py

--- Python Crawler.py
+++ Python Crawler.py
@@ -923,5 +923,40 @@ def main():
 if __name__ == '__main__':
     main()
 
+#Selenium基本使用    
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
+browser = webdriver.Chrome()
+# browser = webdriver.Firefox()
+# browser = webdriver.Edge()
+# browser = webdriver.Safari()
+# browser = webdriver.PhantomJS()
+try:
+    browser.get('https://www.baidu.com')
+    input = browser.find_element_by_id('kw')
+    input.send_keys('Python')
+    input.send_keys(Keys.ENTER)
+    wait = WebDriverWait(browser, 10)
+    wait.until(EC.presence_of_element_located((By.ID, 'content_left')))
+    print(browser.current_url)
+    print(browser.get_cookies)
+    print(browser.page_source)
+finally:
+    browser.close()
+
+#定位网页元素
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+browser.get('https://www.taobao.com')
+input_first = browser.find_element_by_id('q')
+input_second = browser.find_element_by_css_selector('#q')
+input_third = browser.find_element_by_xpath('//*[@id= "q"]')
+print(input_first, input_second, input_third)
+browser.close() #元素选择需要注意
+#Splash使用
 #第8章验证码识别
